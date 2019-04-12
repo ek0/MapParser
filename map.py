e3ea33c0aa7421b9424fb07fee05a433c5790a8a
ek0 <7302683+ek0@users.noreply.github.com>
2019-04-12 10:42:27 -0400

    Changed output for just a dict

--- map.py
+++ map.py
@@ -1,16 +1,19 @@
-class MapEntry():
-    def __init__(self, address, name):
-        self.address = address
-        self.name = name
-
-    def __repr__(self):
-        return "{}: {}".format(self.address, self.name)
+#class MapEntry():
+#    def __init__(self, address, name):
+#        self.address = address
+#        self.name = name
+#
+#    def __repr__(self):
+#        return "MapEntry({:#08x}, \"{}\")".format(self.address, self.name)
+#
+#    def __str__(self):
+#        return self.name
 
 
 class MapFile():
     def __init__(self, path):
         self.index = 0
-        self.functions = []
+        self.functions = {}
         f = open(path, "r")
         while f.readline()[:9] != "  Address":
             continue
@@ -19,8 +22,8 @@ class MapFile():
         while tmp:
             if tmp == "\n":
                 break
-            entry = MapEntry(int(tmp.split()[2], 16), tmp.split()[1])
-            self.functions.append(entry)
+            self.functions[int(tmp.split()[2], 16)] = tmp.split()[1]
+            #self.functions.append(entry)
             tmp = f.readline()
 
     def __iter__(self):
@@ -30,16 +33,20 @@ class MapFile():
         return next(self.functions)
 
     def __getitem__(self, key):
-        if type(key) == int:
-            for i in self.functions:
-                if i.address == key:
-                    return i
-        if type(key) == str:
-            for i in self.functions:
-                if i.name == key:
-                    return i
-        else:
-            raise TypeError
+        return self.functions[self.__keytransform__(key)]
+        #if type(key) == int or type(key) == long:
+        #    for i in self.functions:
+        #        if i.address == key:
+        #            return i
+        #if type(key) == str:
+        #    for i in self.functions:
+        #        if i.name == key:
+        #            return i
+        #else:
+        #    raise TypeError
 
     def __len__(self):
         return len(self.functions)
+    
+    def __keytransform__(self, key):
+        return key
