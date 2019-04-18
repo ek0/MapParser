378f69b7a1ec1cef6eea81827638d93c04c2ce62
ek0 <7302683+ek0@users.noreply.github.com>
2019-04-18 17:24:17 -0400

    Add IDA UI features

--- map.py
+++ map.py
@@ -1,14 +1,29 @@
-#class MapEntry():
-#    def __init__(self, address, name):
-#        self.address = address
-#        self.name = name
-#
-#    def __repr__(self):
-#        return "MapEntry({:#08x}, \"{}\")".format(self.address, self.name)
-#
-#    def __str__(self):
-#        return self.name
+import idc
+import idautils
+import idaapi
+import ida_ua
 
+get_symbol = "helper:get_symbol"
+# Class listening to UI notification
+hooks = None
+map_file = None
+
+class HelperGetSymbol(idaapi.action_handler_t):
+        def __init__(self):
+            idaapi.action_handler_t.__init__(self)
+
+        def update(self, ctx):
+            return idaapi.AST_ENABLE_FOR_FORM if ctx.form_type == idaapi.BWN_DISASM else idaapi.AST_DISABLE_FOR_FORM
+
+        def activate(self, ctx):
+            srva = get_name_ea(here(), idaapi.get_highlighted_identifier())
+            print(srva)
+            MakeName(srva, map_file[srva].replace("~", "::Destruct"))
+
+class Hooks(idaapi.UI_Hooks):
+    def finish_populating_tform_popup(self, form, popup):
+        if idaapi.get_tform_type(form) == idaapi.BWN_DISASM:
+            idaapi.attach_action_to_popup(form, popup, get_symbol, None)
 
 class MapFile():
     def __init__(self, path):
@@ -50,3 +65,19 @@ class MapFile():
     
     def __keytransform__(self, key):
         return key
+
+def load_map(path):
+    idaapi.register_action(idaapi.action_desc_t(get_symbol, "Get Symbol", HelperGetSymbol(), "", "Get the symbol for address and copy it to clipboard"))
+    global hooks
+    global map_file
+    map_file = MapFile(path)
+    if hooks is None:
+        hooks = Hooks()
+        hooks.hook()
+
+def unload_map():
+    idaapi.unregister_action(get_symbol)
+    global hooks
+    if hooks is not None:
+        hooks.unhook()
+        hooks = None
