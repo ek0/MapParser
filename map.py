4ff481b235ce9332fc6be1babb95f8f1d205c9a4
ek0 <7302683+ek0@users.noreply.github.com>
2019-10-23 22:15:38 -0400

    Add chooser actions

--- map.py
+++ map.py
@@ -1,12 +1,11 @@
-#import idc
-#import idautils
-#import ida_kernwin
 import ida_kernwin
+import ida_bytes
+import ida_name
 
 # Class listening to UI notification
 hooks = None
 map_file = {}
-
+mapfile_chooser = None
 
 class MapFileMenuHandler(ida_kernwin.action_handler_t):
     """
@@ -25,9 +24,27 @@ class MapFileMenuHandler(ida_kernwin.action_handler_t):
         print("MapParser: Loading MAP file...")
         load_map(path)
         print("MapParser: MAP file loaded.")
+        global mapfile_chooser
+        mapfile_chooser = MapFileChooser()
+        mapfile_chooser.Show()
+
+class MapFileShowSymbols(ida_kernwin.action_handler_t):
+    """
+        Menu handler. File/Load file/Load MAP...
+    """
+    def __init__(self):
+        ida_kernwin.action_handler_t.__init__(self)
+
+    def update(self, ctx):
+        return ida_kernwin.AST_ENABLE_ALWAYS
+
+    def activate(self, ctx):
+        if map_file is None:
+            print("MapParser: No MAP file loaded...")
+        mapfile_chooser.Show()
 
 
-class MapFileRenameSymbolHandler(ida_kernwin.action_handler_t):
+class MapFileRenameSymbolAtCursorHandler(ida_kernwin.action_handler_t):
     """
         Contextual menu for renaming functions
     """
@@ -44,15 +61,37 @@ class MapFileRenameSymbolHandler(ida_kernwin.action_handler_t):
         if len(map_file) is 0:
             # No symbol loaded, aborting
             return
-        srva = int(get_name_ea(here(), ida_kernwin.get_highlight(ida_kernwin.get_current_viewer())[0]))
-        idc.set_name(srva, map_file[srva].replace("~", "::Destruct"), SN_CHECK)
+        srva = int(ida_name.get_name_ea(here(), ida_kernwin.get_highlight(ida_kernwin.get_current_viewer())[0]))
+        ida_name.set_name(srva, map_file[srva].replace("~", "::Destruct"), ida_name.SN_CHECK)
         ida_kernwin.request_refresh(ida_kernwin.IWID_DISASMS)
+
+
+class MapFileRenameSymbolHandler(ida_kernwin.action_handler_t):
+    """
+        Contextual menu for renaming functions
+    """
+    def __init__(self):
+        ida_kernwin.action_handler_t.__init__(self)
+
+    def update(self, ctx):
+        return ida_kernwin.AST_ENABLE_FOR_WIDGET
+
+    def activate(self, ctx):
+        if len(map_file) is 0:
+            # No symbol loaded, aborting
+            return
+        for i in mapfile_chooser.GetSelectedItems():
+            srva = int(mapfile_chooser.GetLine(i)[0], 16)
+            name = mapfile_chooser.GetLine(i)[2]
+            print("{} {}".format(srva, name))
+            ida_name.set_name(srva, name.replace("~", "::Destruct"), ida_name.SN_CHECK)
+            ida_kernwin.request_refresh(ida_kernwin.IWID_DISASMS)
 
 
 class Hooks(ida_kernwin.UI_Hooks):
     def finish_populating_widget_popup(self, form, popup):
         if ida_kernwin.get_widget_type(form) == ida_kernwin.BWN_DISASM:
-            ida_kernwin.attach_action_to_popup(form, popup, "mapfile:rename_symbol", None)
+            ida_kernwin.attach_action_to_popup(form, popup, "mapfile:rename_symbol_at_cursor", None)
 
 
 def load_map(path):
@@ -70,14 +109,17 @@ def load_map(path):
 
 
 class MapFileChooser(ida_kernwin.Choose):
-    def __init__(self, title):
+    def __init__(self):
         self.items = []
         self.selected_items = []
         columns = [["Address", 18], ["Current Name", 50], ["Symbol Name", 50]]
-        ida_kernwin.Choose.__init__(self, title, columns, ida_kernwin.Choose.CH_MULTI)
+        ida_kernwin.Choose.__init__(self, "Symbols", columns, ida_kernwin.Choose.CH_MULTI)
         for key, val in map_file.items():
-            line = ["{:#016X}".format(key), get_name(key), val]
+            line = ["{:#016X}".format(key), ida_name.get_name(key), val]
             self.items.append(line)
+
+    def OnPopup(self, form, popup_handle):
+        ida_kernwin.attach_action_to_popup(form, popup_handle, "mapfile:rename_symbol", None)
 
     def OnGetLine(self, idx):
         return self.items[idx]
@@ -85,26 +127,46 @@ class MapFileChooser(ida_kernwin.Choose):
     def OnGetSize(self):
         return len(self.items)
 
+    def OnSelectLine(self, n):
+        ea = int(self.items[n[0]][0], 16)
+        if ida_bytes.is_mapped(ea):
+            ida_kernwin.jumpto(ea)
+
+    def OnSelectionChange(self, sel_list):
+        self.selected_items = sel_list
+
+    def GetSelectedItems(self):
+        return self.selected_items
+
+    def GetLine(self, idx):
+        return self.items[idx]
+
 
 def unload():
     """
         If you ever need to clean up
     """
-    ida_kernwin.unregister_action("mapfile:rename_symbol")
+    ida_kernwin.unregister_action("mapfile:rename_symbol_at_cursor")
     ida_kernwin.unregister_action("mapfile:load_map")
+    ida_kernwin.unregister_action("mapfile:view_symbols")
+    ida_kernwin.unregister_action("mapfile:rename_symbol")
     global hooks
     global map_file
+    global mapfile_chooser
     if hooks is not None:
         hooks.unhook()
         hooks = None
     del map_file
-    map_file = None
+    del mapfile_chooser
 
 
 def register_actions():
-    ida_kernwin.register_action(ida_kernwin.action_desc_t("mapfile:rename_symbol", "Get Symbol", MapFileRenameSymbolHandler(), "", "Get the symbol for a given address"))
+    ida_kernwin.register_action(ida_kernwin.action_desc_t("mapfile:rename_symbol_at_cursor", "Get Symbol", MapFileRenameSymbolAtCursorHandler(), "", "Get the symbol for a given address"))
     ida_kernwin.register_action(ida_kernwin.action_desc_t("mapfile:load_map", "Load MAP...", MapFileMenuHandler(), "", "Load map file"))
+    ida_kernwin.register_action(ida_kernwin.action_desc_t("mapfile:view_symbols", "MAP Symbols", MapFileShowSymbols(), "", "View map file symbols"))
+    ida_kernwin.register_action(ida_kernwin.action_desc_t("mapfile:rename_symbol", "Rename/Create symbol at address", MapFileRenameSymbolHandler(), "", "Rename/Create symbol at address"))
     ida_kernwin.attach_action_to_menu("File/Load file/", "mapfile:load_map", ida_kernwin.SETMENU_APP)
+    ida_kernwin.attach_action_to_menu("View/Open subviews/", "mapfile:view_symbols", ida_kernwin.SETMENU_APP)
     global hooks
     if hooks is None:
         hooks = Hooks()
@@ -112,3 +174,4 @@ def register_actions():
 
 if __name__ == "__main__":
     register_actions()
+
